1b473cfa6bce6e7fed47c16fe65018cac6cf691a
fhssouza <fhssouza@gmail.com>
2021-02-23 19:40:11 -0300

    Atualizar Projeto

--- routes/products.js
+++ routes/products.js
@@ -3,11 +3,47 @@ const model = require('../model/products')
 
 var router = express.Router();
 
+const products = [{
+  name: 'geladeira',
+  price: 800,
+  type: 'eletronic'
+}, {
+  name: 'arroz',
+  price: 30,
+  type: 'food'
+}];
+
+const types = [{
+  id: "eletronic",
+  label: "Eletronico"
+}, {
+  id: "food",
+  label: "Alimenticio"
+}, {
+  id: "drugs",
+  label: "Farmaceutico"
+}]
+
 // http://localhost:3000/products
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('products', { title: 'produtos', products: model.getProducts() });
+  res.render('products', { name: "Página de Produtos", types: types});
 });
+
+//req = requisição
+//req = resposta
+
+router.post('/', function(req, res){
+  console.log(req.body)
+  res.render('products', { name: "Página de Produtos", types: types});
+});
+
+// http://localhost:3000/products/lista
+
+router.get('/lista', function(req, res){
+  res.render('productsList', {products: products});
+});
+
 
 module.exports = router;
